5596711de21b47b4dac126ac8aa2b371e5604dbf
Ian <brown.i18@sky.com>
2016-09-15 13:50:09 +0100

    Added Styling to the Into

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -3,13 +3,17 @@ var gulp = require('gulp'),
   autoprefixer = require('autoprefixer'),
   cssVars = require('postcss-simple-vars'),
   nested = require('postcss-nested'),
-  cssImport = require('postcss-import');
+  cssImport = require('postcss-import'),
+  mixins = require('postcss-mixins');
 
 
 gulp.task('styles', function() {
 
   return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([cssImport, nested, cssVars, autoprefixer]))
-    .pipe(gulp.dest('./app/temp/styles'));
+    .pipe(postcss([cssImport, mixins, nested, cssVars, autoprefixer]))
+    .on('error', function(errorInfo) {
+      console.log(errorInfo.toString());
+      this.emit('end');
+    }).pipe(gulp.dest('./app/temp/styles'));
 
 });
